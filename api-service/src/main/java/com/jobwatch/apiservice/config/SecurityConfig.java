d3c8eddffc91e43f52de6a33d238f00dd70b49c8
Patrick Xiao <pdxiao2@illinois.edu>
2026-03-17 17:17:42 -0700

    feat: add HTTPS

--- api-service/src/main/java/com/jobwatch/apiservice/config/SecurityConfig.java
+++ api-service/src/main/java/com/jobwatch/apiservice/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://3.150.197.180:*"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://3.150.197.180:*", "https://jobwatch.duckdns.org"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
